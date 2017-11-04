d3e330d1dbab0f8fc50e37b5a6f691892fc4e618
BPYap <32807097+BPYap@users.noreply.github.com>
2017-11-04 21:35:16 +0800

    Add movie function for staff is completed

--- src/Moblima.java
+++ src/Moblima.java
@@ -8,7 +8,7 @@ public class Moblima {
 			System.out.println("\n(1)Customer");
 			System.out.println("(2)Staff");
 			System.out.println("(3)Exit");
-			System.out.println("\nEnter the number of your choice: ");
+			System.out.println("\nChoose an option: ");
 			stafforcust=sc.nextInt();
 			
 			switch(stafforcust) {
@@ -53,50 +53,58 @@ public class Moblima {
 					}while(choice<10);
 					break;
 				case 2:
-					int choice2;
-					do {
-						System.out.println("\n(1)Login");
-						System.out.println("(2)Add Movie");
-						System.out.println("(3)Set Movie Status");
-						System.out.println("(4)Add Special Date");
-						System.out.println("(5)Edit Special Date");
-						System.out.println("(6)Edit Ticket Price");
-						System.out.println("(7)Create Movie Show Time");
-						System.out.println("(8)Show All Movie");
-						System.out.println("(9)Show All Special Date");
-						System.out.println("(10)Show All Ticket Price");
-						System.out.println("(11)Show All Movie Show Time");
-						System.out.println("(12)Exit");
-						System.out.println("\nEnter the number of your choice: ");
-						choice2=sc.nextInt();
-					}while(choice2<12);
+                    System.out.println("\nPlease login.");   
+                    CinemaStaff.login();
+                    if(CinemaStaff.login_status == false)
+                    {
+                        stafforcust = -1;
+                        break;
+                    }
 					
-					switch(choice2) {
-					case 1:
-						break;
-					case 2:
-						break;
-					case 3:
-						break;
-					case 4:
-						break;
-					case 5:
-						break;
-					case 6:
-						break;
-					case 7:
-						break;
-					case 8:
-						break;
-					case 9:
-						break;
-					case 10:
-						break;
-					case 11:
-						break;
-					case 12:
-						break;
-				}
+                    int choice2;
+					do {
+                        utility.printBorder();
+						System.out.println("(1)Add Movie");
+						System.out.println("(2)Set Movie Status");
+						System.out.println("(3)Add Special Date");
+						System.out.println("(4)Edit Special Date");
+						System.out.println("(5)Edit Ticket Price");
+						System.out.println("(6)Create Movie Show Time");
+						System.out.println("(7)Show All Movie");
+						System.out.println("(8)Show All Special Date");
+						System.out.println("(9)Show All Ticket Price");
+						System.out.println("(10)Show All Movie Show Time");
+						System.out.println("(11)Exit");
+						System.out.println("\nChoose an option: ");
+						choice2=sc.nextInt();
+                        switch(choice2) {
+                        case 1:
+                            CinemaStaff.addMovie();
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;
+                        case 9:
+                            break;
+                        case 10:
+                            break;
+                        case 11:
+                            break;
+                    }
+                        }while(choice2 < 11);
+					
+
 					break;
 				}
 			}while(stafforcust<3);
