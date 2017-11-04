26f9146e316ae7f6adddb1dcc5544f7dec95d4ef
harikap79 <32979292+harikap79@users.noreply.github.com>
2017-11-04 14:30:14 +0800

    Add files via upload

---
+++ Review.java
@@ -0,0 +1,36 @@
+package movie;
+
+public class Review {
+   private String comments;
+   private int rating;
+   private String reviewer;
+  
+   private String movie_title;
+   
+   public Review( String movie_title,String reviewer, String comments,int rating) {
+	   this.movie_title = movie_title;
+	   this.reviewer = reviewer;
+	   this.comments = comments;
+	   this.rating = rating;
+	   
+   }
+   public String getMovieTitle() {
+	   return this.movie_title;
+	   
+   }
+   public String getReviewer() {
+	   return this.reviewer;
+   }
+   public String getComments() {
+	   return this.comments;
+   }
+   public int getRating() {
+	   return this.rating;
+   }
+   public String toString() {
+	   return String.format("Reviewer " + this.reviewer +
+			  " ,Comments " + this.comments +
+			  ",Rating: " + this.rating);
+			      
+   }
+}
