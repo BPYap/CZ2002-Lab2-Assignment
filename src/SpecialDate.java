7cf324ef1f48bdf791a8a3fdb91a0ebf1cfd45e5
BPYap <32807097+BPYap@users.noreply.github.com>
2017-11-08 09:26:11 +0800

    updated class diagram

--- src/SpecialDate.java
+++ src/SpecialDate.java
@@ -3,15 +3,13 @@ public class SpecialDate {
 	private int month;
 	private int day;
 	private double discount;
-	private String[] affectedType;
 	private String remark;
 	
-	public SpecialDate(int year, int month, int day, double discount, String[] affectedType, String remark) {
+	public SpecialDate(int year, int month, int day, double discount, String remark) {
 		this.month=month;
 		this.year=year;
 		this.day=day;
 		this.discount=discount;
-		this.affectedType=affectedType;
 		this.remark=remark;
 	}
 }
