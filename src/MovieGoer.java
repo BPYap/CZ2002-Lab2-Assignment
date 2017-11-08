2981cc9fb38f84985c4c0d738fd7ca5615028ed7
BPYap <32807097+BPYap@users.noreply.github.com>
2017-11-08 14:44:41 +0800

    rename

--- src/MovieGoer.java
+++ src/MovieGoer.java
@@ -3,56 +3,22 @@ import java.util.*;
 public class MovieGoer {
 	public static Scanner sc = new Scanner(System.in);
     
-    public static void reviewMovie(){
-        System.out.println("========== Review Movie ==========");
-        System.out.println("Which movie you want to review");
-        String movie = listAllMovies();
-        System.out.print("Enter your comment: ");
-        String dummy = sc.nextLine();
-        String comment = sc.nextLine();
-        System.out.print("Enter your name: ");
-        String reviewer = sc.nextLine();
-        System.out.print("Enter your rating (1-10): ");
-        int rating;
-        do{
-            rating = sc.nextInt();
-        }while(rating<0 || rating >10);
-        
-        Review review = new Review(movie, reviewer, comment, rating);
-        String record = review.toString();
-        utility.addRecord("review.txt",record);
-        System.out.println("Your review is added into review database");
-    }
-    
-    public static void listReview(){
-        String movie;
-        movie = listAllMovies();
-        Review[] reviews = Database.read_review();
-        
-        for(int i=0;i<reviews.length;i++){
-            if(reviews[i].getMovieTitle().equals(movie)){
-                String row = "Movie Title : "+reviews[i].getMovieTitle()+"\n"+"Reviewer :    "+reviews[i].getReviewer()+"\n"+"Comment :    "+reviews[i].getComments()+"\n"+"Rating :    "+reviews[i].getRating();
-                System.out.println(row);
-            }
-        }
-    }
-    
-    public static String listAllMovies(){
-        String movie;
-        
+    public static String selectMovieTitle(){
         Movie[] movies = Database.read_movie(false);
         
-        for(int i=0;i<movies.length;i++){
+        for(int i = 0; i < movies.length; i++){
             int k=i+1;
-            System.out.println("("+k+") : "+movies[i].getMovieTitle());
+            System.out.println("("+k+") : "+ movies[i].getMovieTitle());
         }
-        
+        System.out.println("Select the index of a movie to review");
         int choice = 0;
-        choice = sc.nextInt();
+        do
+        {
+            choice = sc.nextInt();
+        }while(choice <= 0 || choice > movies.length);
+        sc.nextLine();
         
-        movie=movies[choice-1].getMovieTitle();
-        
-        return movie;
+        return movies[choice-1].getMovieTitle();
     }
     
 	public static void listMovies() 
@@ -84,5 +50,37 @@ public class MovieGoer {
         System.out.println("Synopsis   : " + movies[choice-1].getSynopsis());
         System.out.println("Director   : " + movies[choice-1].getDirector());
         System.out.println("Cast       : " + movies[choice-1].getCast());
+    }
+    
+    public static void reviewMovie(){
+        System.out.println("========== Review Movie ==========");
+        String movie_title = selectMovieTitle();
+        System.out.print("Enter your name: ");
+        String reviewer = sc.nextLine();
+        System.out.print("Enter your comment: ");
+        String comment = sc.nextLine();
+        System.out.print("Enter your rating (1-10): ");
+        int rating;
+        do{
+            rating = sc.nextInt();
+        }while(rating <= 0 || rating >10);
+        sc.nextLine();
+        
+        Review review = new Review(movie_title, reviewer, comment, rating);
+        String record = review.toString();
+        utility.addRecord("review.txt",record);
+        System.out.println("Your review has been added into review database");
+    }
+    
+    public static void listReview(){
+        String movie_title = selectMovieTitle();
+        Review[] reviews = Database.read_review();
+        
+        for(int i=0;i<reviews.length;i++){
+            if(reviews[i].getMovieTitle().equals(movie_title)){
+                String row = "Movie Title : "+reviews[i].getMovieTitle()+"\n"+"Reviewer :    "+reviews[i].getReviewer()+"\n"+"Comment :    "+reviews[i].getComments()+"\n"+"Rating :    "+reviews[i].getRating();
+                System.out.println(row);
+            }
+        }
     }
 }
