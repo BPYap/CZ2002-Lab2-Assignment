32bad5a56f29add1a4ac4d4468896b73d0ffdc04
BPYap <32807097+BPYap@users.noreply.github.com>
2017-11-09 19:50:41 +0800

    111

--- src/Cineplex.java
+++ src/Cineplex.java
@@ -13,12 +13,6 @@ public class Cineplex {
 	public Cineplex(String record)
     // format: number_of_cinema|Location|Cinema_Code|Cinema_Class|Number_of_Rows|Number_of_Columns
     {
-<<<<<<< HEAD
-        Object [] attributes = record.split("\\|");
-        number_of_cinema = (Integer) attributes[0];
-        location = (String)attributes[1];
-        cinema = (cinema)attributes[2];
-=======
         String [] attributes = record.split("\\|");
         number_of_cinema = Integer.parseInt(attributes[0]);
         location = attributes[1];
@@ -33,7 +27,6 @@ public class Cineplex {
             cinema[i] = new Cinema(cinema_code, cinema_class, row, column);
             temp += 4;
         }
->>>>>>> 2d707c79b6701b45c0d63a67b250bb398427d4ed
     }
 	
 	public String getLocation() {
@@ -53,5 +46,6 @@ public class Cineplex {
                 return this.cinema[i];
             }
         }
+        return null;
     }
 }
