2340238b4e24184aafb4141d6cbf9833ec1d275e
Shantanu Sharma <shantanu1798@gmail.com>
2017-11-09 21:41:21 +0800

    Update SpecialDate.java

--- src/SpecialDate.java
+++ src/SpecialDate.java
@@ -1,16 +1,16 @@
 public class SpecialDate {
-	private int year;
-	private int month;
-	private int day;
-	private double discount;
-	private String remark;
+    private int year;
+    private int month;
+    private int day;
+    private double discount;
+    private String remark;
 	
-	public SpecialDate(int year, int month, int day, double discount, String remark) {
-		this.month = month;
-		this.year = year;
-		this.day = day;
-		this.discount = discount;
-		this.remark = remark;
+    public SpecialDate(int year, int month, int day, double discount, String remark) {
+	this.month = month;
+	this.year = year;
+	this.day = day;
+	this.discount = discount;
+	this.remark = remark;
 	}
     
     public SpecialDate(String record)
