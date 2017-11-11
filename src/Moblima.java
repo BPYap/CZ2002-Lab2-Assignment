f2b8a2097f38b7c8f0f43c8f257aa7d4528549a4
moxuan <32980496+LouisHow@users.noreply.github.com>
2017-11-11 17:13:00 +0800

    asd

--- src/Moblima.java
+++ src/Moblima.java
@@ -65,7 +65,7 @@ public class Moblima {
                                 MovieGoer.bookTickets();
 								break;
 							case 6:
-                                MovieGoer.checkTransactionHistory();
+                                //MovieGoer.checkTransactionHistory();
 								break;
 							case 7:
                                 MovieGoer.listTopSalesMovies();
