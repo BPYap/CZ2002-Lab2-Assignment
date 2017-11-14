de63e7bacea99b3ce80cc9167d3f5b2ca396b9c7
BPYap <32807097+BPYap@users.noreply.github.com>
2017-11-14 22:45:50 +0800

    Mr Bean bought a ticket for PVZ

--- src/MovieGoer.java
+++ src/MovieGoer.java
@@ -214,7 +214,7 @@ public class MovieGoer {
 
         //print showtime to select
         String widths = "20,20,30,20,20";
-        utility.print_title_row("Date,Showtime,Cinema Class,Discount,Promotion", widths);
+        utility.print_title_row("Date,Showtime,Cinema Class,Promotion,Discount", widths);
         SpecialDate[] special_dates = Database.read_special_date();
         String discount;
         String promotion;
@@ -233,7 +233,7 @@ public class MovieGoer {
                     break;
                 }
             }
-            String row = year+"/"+month+"/"+day+","+Integer.toString(showtime[i].getStartTime()) + "," + cineplex.getCinema(showtime[i].getCinemaCode()).getCinemaClass() + "," + discount + "," + promotion;
+            String row = year+"/"+month+"/"+day+","+Integer.toString(showtime[i].getStartTime()) + "," + cineplex.getCinema(showtime[i].getCinemaCode()).getCinemaClass() + "," + promotion + "," + discount;
             utility.print_row(i+1, row, widths);
         }
         
